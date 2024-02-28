1e4f1fd3f7512deda3344576b9f0910e43180508
Leonardo Nava <L3oNav@outlook.com>
2024-02-28 12:27:53 -0800

    Fix socket import and add server response handling

--- app/main.py
+++ app/main.py
@@ -7,9 +7,13 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    
+    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, address = server_socket.accept() # wait for client
+
+    with connection:
+        connection.recv(1024)
+        connection.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
