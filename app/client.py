f1e9ff5bcf9d8e401a79399d23627bced83895e3
Leonardo Nava <L3oNav@outlook.com>
2024-02-28 13:45:30 -0800

    stage 4 pass

--- app/client.py
+++ app/client.py
@@ -8,6 +8,20 @@ class RedisClient:
        self.sock = socket
        self.address = address
 
+    def parse_resp_command(data):
+        lines = data.split("\r\n")
+        command = None
+        arguments = []
+        if lines[0].startswith("*"):
+            num_elements = int(lines[0][1:])
+            for i in range(1, num_elements * 2, 2):
+                if lines[i].startswith("$"):
+                    arguments.append(lines[i + 1])
+            if arguments:
+                command = arguments[0].upper()
+                arguments = arguments[1:]
+        return command, arguments
+
     def recv(self):
         return self.sock.recv(1024)
 
@@ -16,10 +30,14 @@ class RedisClient:
     
     def hc(self):
         while True:
-            data = self.recv()
-            print("->",data)
-            if data == b"*1\r\n$4\r\nping\r\n":
+            cmmd, args = self.parse_resp_command(self.recv()) 
+            if cmmd == "PING":
                 self.send("+PONG\r\n")
+            #echo
+            if cmmd == "ECHO" and args:
+                message = args[0]
+                response = f"${len(message)}\r\n{message}\r\n"
+                self.send(response)
             if not data:
                 break
         self.sock.close()
